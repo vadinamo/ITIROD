1c7681c3587213ac421cc945f391537ed9932673
vadinamo <vadinamo@gmail.com>
2023-04-13 19:06:33 +0300

    bit of class renaming

--- lab3/scripts/nav-dropdown.js
+++ lab3/scripts/nav-dropdown.js
@@ -1,5 +1,5 @@
 function myFunction() {
-    element = document.getElementById("dropdown-content")
+    element = document.getElementById("dropdown__content")
     display = element.style.display
     if (display === 'grid') {
         element.style.display = 'none'
